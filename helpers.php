551b4922566884d18cc540d70b92689a101def79
intermedia <intermediacolombia@gmail.com>
2026-08-05 23:19:16 -0500

    Quitar log de debug de apiGetLid

--- helpers.php
+++ helpers.php
@@ -309,7 +309,6 @@ function apiGetLid(string $phone): ?string
     curl_close($ch);
     if ($curlErr) { error_log('[apiGetLid] curl error: ' . $curlErr); return null; }
     $decoded = json_decode($response, true);
-    error_log('[apiGetLid] phone=' . $phone . ' response=' . $response);
     return ($decoded['success'] && $decoded['found']) ? $decoded['lid'] : null;
 }
 
